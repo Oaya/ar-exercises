fe7d8a3cdcb116a3d599ae5e0058cf4276ff29c6
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-03-08 05:14:57 +0000

    has_mens_or_womens_apparel validate

--- lib/store.rb
+++ lib/store.rb
@@ -1,14 +1,13 @@
 class Store < ActiveRecord::Base
   has_many :employees
-  validate :has_mens_or_womens_apparel
+ 
   validates :name, presence: true, length: { minimum: 3 }
   validates :annual_revenue, presence: true, numericality: { greater_than_or_equal_to: 0 }
- 
+  validate :has_mens_or_womens_apparel
   
 
   def has_mens_or_womens_apparel
-    # puts "Hi"
-    # puts "#{mens_apparel},#{womens_apparel}"
+ 
     if (mens_apparel == false || mens_apparel == nil) && (womens_apparel == false || womens_apparel == nil)
       errors.add(:has_mens_or_womens_apparel, "should have carry men's or women's apparel")
     end
