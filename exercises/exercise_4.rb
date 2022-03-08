3237613a5eafe94a198e54389d19e3ea3019ed37
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-03-08 01:59:00 +0000

    create 3 more new store data and get by annual_revenue and apparels

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,19 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create([{name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true },
+  {name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false },
+  {name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true }] )
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |i|
+  puts "#{i.name}, #{i.annual_revenue}"
+end
+
+@womens_store = Store.where('annual_revenue >= ?', 1000000 ).where(womens_apparel: true)
+
+@womens_store.each do |i|
+  puts "#{i.name}, #{i.annual_revenue}"
+end
