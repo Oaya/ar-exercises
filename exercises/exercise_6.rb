45a374d8d6bc41600beeeac591062e69c42fc995
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-03-08 02:30:11 +0000

    connect store and employee database and create new employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,20 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+ @store1 = Store.find(1)
+ @store2 = Store.find(2)
+ @store4 = Store.find(4)
+
+
+@store1.employees.create([{first_name: "Khurram", last_name: "Virani", hourly_rate: 60},
+                          {first_name: "Tonny", last_name: "Smith", hourly_rate: 45},
+                          {first_name: "Kim", last_name: "Jisang", hourly_rate: 50}])
+
+@store2.employees.create([{first_name: "Tom", last_name: "Cruis", hourly_rate: 100},
+                          {first_name: "Cameron", last_name: "Paul", hourly_rate: 30}])
+
+@store4.employees.create({first_name: "Jessica", last_name: "Jones", hourly_rate: 66})
+  
+  
+
