bd61b22fbc19cc318ede212c5328931a86ab186c
Aya Okizaki <aya_japan201@yahoo.co.jp>
2022-03-08 06:10:37 +0000

    create password for employees active record

--- lib/employee.rb
+++ lib/employee.rb
@@ -4,4 +4,10 @@ class Employee < ActiveRecord::Base
   validates :last_name, presence: true
   validates :hourly_rate, presence: true, numericality: { in: 40..200 }
   validates :store_id, presence:true
+
+  after_create do
+    self.password = rand.to_s[2..9] if password.blank?
+    puts self.password
+  end 
+
 end
--- lib/store.rb
+++ lib/store.rb
@@ -4,14 +4,12 @@ class Store < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 3 }
   validates :annual_revenue, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validate :has_mens_or_womens_apparel
-  
+
+  private
 
   def has_mens_or_womens_apparel
- 
     if (mens_apparel == false || mens_apparel == nil) && (womens_apparel == false || womens_apparel == nil)
       errors.add(:has_mens_or_womens_apparel, "should have carry men's or women's apparel")
     end
   end
-
-
 end
